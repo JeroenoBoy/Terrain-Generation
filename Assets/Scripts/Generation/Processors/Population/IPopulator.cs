5d0eeaabeaa65e1460a867d2b834cccac37625b3
Jeroen <jeroen@vandegeest.eu>
2022-12-04 13:07:39 +0100

    Working on trees

---
+++ Assets/Scripts/Generation/Processors/Population/IPopulator.cs
@@ -0,0 +1,7 @@
+﻿namespace Generation.Processors.Population
+{
+    public interface IPopulator
+    {
+        public void Execute(CreateChunkJob job);
+    }
+}
---
+++ Assets/Scripts/Generation/Processors/Population/TreePopulator.cs
@@ -0,0 +1,9 @@
+﻿namespace Generation.Processors.Population
+{
+    public struct TreePopulator : IPopulator
+    {
+        public void Execute(CreateChunkJob job)
+        {
+        }
+    }
+}
